ad8580e6fa722668d934eb0c157c9e5395027e54
Zhuoyuan Lin <winster1851@gmail.com>
2017-01-29 19:26:03 -0800

    unit test

---
+++ app/config/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zlin on 1/29/17.
+ */
+var config = {
+  port: process.env.PORT || 2000,
+  db: process.env.MONGOLAB_URI || "mongodb://localhost/todoapi",
+  test_port: 2001,
+  test_db: "mongodb://localhost/todoapi_test"
+};
+module.exports = config;
